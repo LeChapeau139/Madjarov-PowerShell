276646fe3c0d3c9411f9423a6ddc01507be57f29
LeChapeau139 <lechapeau139@users.noreply.github.com>
2023-01-16 23:34:14 +0100

    Create archive of folder

---
+++ TP1/exo7_folder.ps1
@@ -0,0 +1,15 @@
+# Vérifie s'il y a un paramètre passé en ligne de commande
+if ($args.Count -eq 0) {
+    # Demande le chemin d'accès au répertoire à compresser
+    $directory = Read-Host "Entrez le chemin d'accès au répertoire que vous voulez compresser :"
+} else {
+    $directory = $args[0]
+}
+
+# Crée un répertoire "Archives" s'il n'existe pas
+if (!(Test-Path "Archives")) {
+    New-Item -ItemType Directory -Path "Archives"
+}
+
+# Compresse le répertoire dans une archive ZIP
+Compress-Archive -Path $directory -DestinationPath "Archives\$($directory).zip"
