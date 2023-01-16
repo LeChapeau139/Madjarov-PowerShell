b7aec72bef7f0497a2c51b67446863c2ed3641b1
LeChapeau139 <lechapeau139@users.noreply.github.com>
2023-01-16 23:22:03 +0100

    Automatic save

--- TP1/exo8.ps1
+++ TP1/exo8.ps1
@@ -1,4 +1,40 @@
-#Affecter à la variable $password, le mot de passe "motdepasse", et le crypter avec "-AsSecureString"
-$mon_password = ConvertTo-SecureString "motdepasse" -AsSecureString
-#Créer un nouvel utilisateur avec comme nom "new_quentin_user", et le mot de passe défini juste avant
-New-LocalUser -Name "new_quentin_user" -Password $mon_password
+# Récupération des arguments
+$path = $args[0]
+$intervalle = $args[1]
+
+# Vérification de la présence des arguments
+if ($path -eq $null) {
+    Write-Output "Absence de fichier / dossier à sauvegarder"
+    exit
+}
+if ($intervalle -eq $null) {
+    Write-Output "Absence d’intervalle de sauvegarde"
+    exit
+}
+
+# Vérification de la nature de l'élément passé en argument
+if (Test-Path -PathType Leaf $path) {
+    Write-Output "Fichier passé en argument"
+} elseif (Test-Path -PathType Container $path) {
+    Write-Output "Dossier passé en argument"
+}
+
+# Vérification de la valeur de l'intervalle
+if ($intervalle -lt 30) {
+    Write-Output "Intervalle trop petit"
+    exit
+} else {
+    Write-Output "La valeur de l’intervalle est $intervalle secondes"
+}
+
+# Récupération du chemin du répertoire personnel de l'utilisateur connecté
+$userPath = [Environment]::GetFolderPath("Personal")
+
+# Boucle de sauvegarde
+while ($true) {
+    # Copie de l'élément à sauvegarder dans le répertoire personnel
+    Copy-Item -Path $path -Destination $userPath -Recurse
+
+    # Temporisation avant la prochaine sauvegarde
+    Start-Sleep -Seconds $intervalle
+}
