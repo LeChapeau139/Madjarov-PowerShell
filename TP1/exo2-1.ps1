e6f4794cdcaae597213ea4a2cb6b8c66b7626f4d
LeChapeau139 <lechapeau139@users.noreply.github.com>
2023-01-16 23:08:59 +0100

    Print username on shell

--- TP1/exo2-1.ps1
+++ TP1/exo2-1.ps1
@@ -1,4 +1,4 @@
-#Créer une variable username, puis demande sur le shell :
+#Créer une variable username, puis demande sur le shell:
 $username = Read-Host "Entrez votre nom d'utilisateur"
 #Renvoie sur le powershell la phrase + la variable
 Write-Host "Le nom d'utilisateur que vous avez choisi est : $username"
