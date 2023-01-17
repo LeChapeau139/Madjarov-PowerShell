9768fb3934c6b95def996e6159300eaa9c45e246
LeChapeau139 <lechapeau139@users.noreply.github.com>
2023-01-17 19:21:51 +0100

    Update exo2-2.ps1

--- TP1/exo2-2.ps1
+++ TP1/exo2-2.ps1
@@ -4,5 +4,7 @@ $username = Read-Host "Entrez votre nom d'utilisateur"
 $password = Read-Host -AsSecureString "Entrez votre mot de passe"
 #Fait en sorte que le mot de passe sorte en clair, et non en crypté
 $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+#Renvoie sur le powershell la phrase + la variable du nom
 Write-Host "Le nom d'utilisateur que vous avez choisi est : $username"
+#Renvoie sur le powershell la phrase + la variable du mdp
 Write-Host "Et le mot de passe qui correspond : $password"
