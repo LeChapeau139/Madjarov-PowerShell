260b7f1b93069ff2a56b50306f8501c08d89b9e4
LeChapeau139 <lechapeau139@users.noreply.github.com>
2023-01-17 02:12:48 +0100

    @IP /  Settings Network / Service Running / Settings Interface

--- TP1/exo3.ps1
+++ TP1/exo3.ps1
@@ -10,12 +10,28 @@ Get-NetAdapter
 $services = Get-Service | Where-Object {$_.Status -eq "Running"}
 #Pour chaque service en cours dans tous les services
 foreach ($service_run in $services) 
-{
+    {
     #On affiche le nom du service
     Write-Host "Nom du service: $($service_run.Name)"
     #On affiche le statut du service
     Write-Host "Statut du service: $($service_run.Status)"
     #On délimite chaque service par "---" 
     Write-Host "---"
-}
+    }
+#---------------------------------------------------------------------------------------------------------------------#
+#Get-NetAdapter nous permet de récupérer les paramètres de la carte réseau
+$interfaces = Get-NetAdapter
+#Pour chaque interface dans tous les paramètres
+foreach ($interface in $interfaces) 
+    {
+    #On affiche le nom de l'interface en précisant le nom
+    Write-Host "Nom de l'interface : $($interface.Name)"
+    #On affiche le nom de l'interface en précisant l'@MAC
+    Write-Host "Adresse MAC : $($interface.MacAddress)"
+    #On affiche le nom de l'interface en précisant le statut de chaque interface
+    Write-Host "Statut : $($interface.Status)"
+    #On délimite chaque interface par "---" 
+    Write-Host "---"
+    }
+
 #---------------------------------------------------------------------------------------------------------------------#
