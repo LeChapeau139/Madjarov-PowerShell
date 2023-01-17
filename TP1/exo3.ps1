b05b8956b2adfb8163c8b9aeafa78c6e4a31371a
LeChapeau139 <lechapeau139@users.noreply.github.com>
2023-01-17 02:06:39 +0100

    fe

--- TP1/exo3.ps1
+++ TP1/exo3.ps1
@@ -2,3 +2,20 @@
 ipconfig | Select-String "IPv4"
 #Afficher les paramètres de la carte réseau 
 Get-NetAdapter
+
+
+
+#---------------------------------------------------------------------------------------------------------------------#
+#Get-Service nous permet de récupérer le status de chaque service actif, lorsqu'ils sont en mode "running" donc marche 
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}
+#Pour chaque service en cours dans tous les services
+foreach ($service_run in $services) 
+{
+    #On affiche le nom du service
+    Write-Host "Nom du service: $($service_run.Name)"
+    #On affiche le statut du service
+    Write-Host "Statut du service: $($service_run.Status)"
+    #On délimite chaque service par "---" 
+    Write-Host "---"
+}
+#---------------------------------------------------------------------------------------------------------------------#
