e378bc3f907c2109604b7e144a59bd8ae07923c5
LeChapeau139 <lechapeau139@users.noreply.github.com>
2023-01-17 01:59:02 +0100

    oui

--- TP1/exo3.ps1
+++ TP1/exo3.ps1
@@ -1,9 +1,4 @@
 #Permettre de récupérer l'adresse IP d'un pc (ifconfig)
-$ipconfig = ipconfig
-# ToString() permet de passer tout en str, puis -split sépare tous les éléments séparé par ":"
-$ip = ($ipconfig | Select-String "IPv4 Address").ToString() -split ":"
-#.Trim() permet de supprimer les espaces en trop
-$ip = $ip[1].Trim()
-#Affiche la variable $ip 
-Write-Host "L'adresse IP de la machine virtuelle est : $ip"
-
+ipconfig | Select-String "IPv4"
+#Afficher les paramètres de la carte réseau 
+Get-NetAdapter
