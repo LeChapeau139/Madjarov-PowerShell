920d05fa96f532a134836bcab6c35d65a18319e9
LeChapeau139 <122629554+LeChapeau139@users.noreply.github.com>
2023-01-20 09:49:43 +0100

    Delete TP1/TP1 directory

--- TP1/TP1/exo2-1.ps1
+++
@@ -1,4 +0,0 @@
-#Créer une variable username, puis demande sur le shell:
-$username = Read-Host "Entrez votre nom d'utilisateur"
-#Renvoie sur le powershell la phrase + la variable
-Write-Host "Le nom d'utilisateur que vous avez choisi est : $username"
--- TP1/TP1/exo9.ps1
+++
@@ -1,5 +0,0 @@
-$username = Read-Host "Entrez le nom de votre nouvel utilisateur "
-$password = Read-Host -AsSecureString "Entrez le mot de passe corespondant"
-$pass_crypt = ConvertTo-SecureString -AsPlainText $password -Force
-New-LocalUser -Name $username -Password $pass_crypt
-Get-LocalUser -Name $username | Select-Object *
